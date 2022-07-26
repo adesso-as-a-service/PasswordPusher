7d3383c5d5a16c47a3d7f61882014fdd9a599fa3
dk3as <107105741+dk3as@users.noreply.github.com>
2022-07-26 16:56:46 +0200

    Create manifest.js
    Gemfile with updated versions of gems

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,41 @@
+source 'http://rubygems.org'
+
+ruby "~> 3.1.0"
+
+gem 'rails', '~> 7.0'
+
+group :development, :test do
+  gem 'pry', '~> 0.12'
+  gem 'pry-byebug', '~> 3.7', :platforms => [ :mri_20, :mri_21, :mri_22 ]
+end
+
+
+
+
+gem 'json', '~>2.6'
+gem 'slim-rails', '~> 3.5'
+gem 'ezcrypto', :git => 'https://github.com/pglombardo/ezcrypto.git'
+#gem 'modernizr-rails', :git => 'https://github.com/russfrisch/modernizr-rails.git'
+gem "high_voltage", '~> 3.1'
+
+gem 'sass-rails', '~> 6.0'
+gem 'coffee-rails', '~> 5.0'
+gem 'uglifier', '~> 4.2'
+
+gem 'foreman', '~> 0.87'
+gem 'unicorn', '~> 6.0'
+gem 'jquery-rails', '~> 4.5'
+gem 'popper_js', '~> 1.14'
+gem 'bootstrap', '~> 4.3'
+gem 'listen', '~> 3.7'
+
+group :production, :test  do
+  gem 'pg', '~>1.4'
+end
+
+
+group :private do
+  gem "sqlite3", '~> 1.4'
+end
+
+gem 'puma', '~> 5.6'
